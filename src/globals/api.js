3c29d77e761dec78cc1a57331b29718352deb161
Yanuar Fa <yanuarfatur41@gmail.com>
2023-12-18 00:30:09 +0700

    added pagination on doctor pages

--- src/globals/api.js
+++ src/globals/api.js
@@ -58,8 +58,8 @@ const deleteData = async (headers, endpoint) => {
   return data;
 };
 
-export const getDoctors = async () => {
-  return fetchData("/doctor");
+export const getDoctors = async (page) => {
+  return fetchData(`/doctor?page=${page}`);
 };
 
 export const getArticleBySlug = async (slug) => {
