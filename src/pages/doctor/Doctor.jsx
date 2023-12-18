f37188f8fede5e562d8da999baae5825db8fee44
OnyanPokon <mohamadrafiqd@gmail.com>
2023-12-18 17:27:59 +0800

    fixing bugs again

--- src/pages/doctor/Doctor.jsx
+++ src/pages/doctor/Doctor.jsx
@@ -10,7 +10,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import HeroDoctor from "../../assets/lottie/Herodoctor.json";
-import ShowDoctor from "./Showdoctor";
+import ShowDoctor from "./ShowDoctor";
 
 const Doctor = () => {
   const [searchTerm, setSearchTerm] = useState("");
